b86f056395def116ff0236fd2307d017d8b84a18
TommyRivera21 <68372501+TommyRivera21@users.noreply.github.com>
2023-01-29 05:01:54 -0500

    frontend

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <>
+      <img src="'../../public/logo.png" alt="Logo Contact"  className="mx-auto my-auto"/>
+
+      <p className="text-center text-italic">Conectando personas</p>
+    </>
+  );
+}
+
+export default Title;
