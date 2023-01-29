aa7631a5a9fcef18d5b099a2e3fc5f14fa283893
TommyRivera21 <68372501+TommyRivera21@users.noreply.github.com>
2023-01-29 06:09:52 -0500

    titleFinalFin

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 function Title() {
   return (
     <>
-      <h1 className="text-center font-bold text-3xl text-cyan-900">Contact App</h1>
-
       <img src="/logo.png" alt="Logo Contact"  className="mx-auto my-auto"/>
 
       <p className="text-center text-italic">Conectando personas</p>
